47b2f1f05042230f8d287442ed1e3b4854562bc6
Jason Jun <junjason@umich.edu>
2023-09-15 14:16:23 -0400

    done with stock picker

--- tdd_practice/lib/tdd.rb
+++ tdd_practice/lib/tdd.rb
@@ -19,4 +19,32 @@ class Array
         end
         sums
     end
+    def my_transpose
+        results = Array.new(self.length) {Array.new(self.length)}
+        self.each_with_index do |el, idx|
+            (0...self.length).each do |jdx|
+                results[jdx][idx] = el[jdx] 
+            end
+        end
+        results
+    end
+end
+
+# Write a method that takes an array of stock prices (prices on days 0, 1, ...),
+# and outputs the most profitable pair of days on which to first buy the stock and 
+# then sell the stock. Remember, you can't sell stock before you buy it!
+
+
+def stock_picker(arr)
+    results = []
+    max_profit = 0
+    arr.each_with_index do |ele, idx|
+        (idx...arr.length).each do |jdx|
+            if arr[jdx]-arr[idx] > max_profit
+                max_profit = arr[jdx]-arr[idx]
+                results = [arr[idx],arr[jdx]]
+            end
+        end
+    end
+    results
 end
--- tdd_practice/spec/tdd_spec.rb
+++ tdd_practice/spec/tdd_spec.rb
@@ -1,17 +1,35 @@
 require "tdd"
 require "rspec"
 describe Array do
-    let(:arr1) {[-1,0,2,-2,1]}
-    let(:arr2) {[[0,4],[2,3]]}
+    
     describe "#uniq" do 
         it "removes duplicates from array correctly" do
             expect([1,2,1,3,3].my_uniq).to eq([1,2,3])
         end
 
     end
+
+    let(:arr1) {[-1,0,2,-2,1]}
+    let(:arr2) {[[0,4],[2,3]]}
     describe "#two_sum" do
         it "correctly finds pairs of positions that sum to zero" do
             expect(arr1.two_sum).to eq(arr2)
         end
+    end
+
+    let(:rows) {[[0,1,2],[3,4,5],[6,7,8]]}
+    let(:cols) {[[0,3,6],[1,4,7],[2,5,8]]}
+    describe "#my_transpose" do 
+        it "will convert row oriented and column oriented representations" do
+            expect(rows.my_transpose).to eq(cols)
+        end
+    end
+end
+
+describe "stock_picker" do
+    let(:stocks) {[1,7,3,5,8,9,0]}
+    let(:profits) {[1,9]}
+    it "will find the pair of days that produce the highest profit" do
+        expect(stock_picker(stocks)).to eq(profits)
     end
 end
